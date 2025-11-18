878aae511f5cd7554eb9986256f5b96d0e6a45e1
ali3v <xalisoglu@gmail.com>
2025-11-18 15:21:13 +0000

    test

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-Class 0-square
-
-This class defines a square.
+This module defines a Square class with a private size attribute.
 """
 
 
 class Square:
-
     """
-    Defines a square.
-
-    Args:
-
-    Raises:
-
-    Returns:
-        None.
+    Represents a square with a private size attribute.
     """
+
+    def __init__(self, size):
+        """
+        Initialize the square with the given size.
+
+        Args:
+            size: The size of the square (no type/value verification yet).
+        """
+        self.__size = size
