147f8a3c6e1822f6382949f454d7634d74b0660a
ali3v <xalisoglu@gmail.com>
2025-11-18 15:55:08 +0000

    test

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,62 +1,27 @@
-#!/usr/bin/python3
-
-"""
-Class 4-square
-
-This class defines a square by a size.
-"""
-
-
 class Square:
-
-    """Defines a square."""
+    """Defines a square"""
 
     def __init__(self, size=0):
-
-        """
-        Initializes the square with a given size.
-
-        Args:
-            size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-
         self.size = size
 
     @property
     def size(self):
-
-        """Returns the size"""
-
         return self.__size
 
     @size.setter
     def size(self, value):
-
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The new size of the square.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
-
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-
         if value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
-
-        """Returns the current square area."""
-
         return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
